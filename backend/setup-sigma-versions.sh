6488bf291a8ada476bc45b8136d63af8918e34f5
M3NIX <julian@ortel.tech>
2025-08-18 16:51:31 +0200

    bump deprecated docker version and exclude sigma-cli release candidate versions

--- backend/setup-sigma-versions.sh
+++ backend/setup-sigma-versions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # fetch 10 latest versions of sigma-cli
-SIGMA_VERSIONS=$(curl -s https://pypi.org/pypi/sigma-cli/json | jq -r '.releases | keys | .[-10:] | .[]')
+SIGMA_VERSIONS=$(curl -s https://pypi.org/pypi/sigma-cli/json | jq -r '.releases | keys | map(select(contains("rc") | not)) | .[-10:] | .[]')
 
 # prepare virtualenv for each version
 for VERSION in $SIGMA_VERSIONS; do
